997bb9a2a8077e4a03bc602ea2143470d0fe4d2b
sanjay-xdr <55450200+sanjay-xdr@users.noreply.github.com>
2020-05-17 16:31:19 +0530

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -8,4 +8,4 @@ if __name__=="__main__":
             timeout=2 #after displaying
             #you can also choose an icon
         )
-        time.sleep(7) #waiting tim
+        time.sleep(7) #waiting time
